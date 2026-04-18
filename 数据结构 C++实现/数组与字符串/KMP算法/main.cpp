616025dd30ff43b25531ec49c7986016ec236148
Tokey <tokeytut0000@outlook.com>
2026-04-18 14:06:35 +0800

    添加了字符串匹配 KMP 算法

--- 数据结构 C++实现/数组与字符串/KMP算法/main.cpp
+++ 数据结构 C++实现/数组与字符串/KMP算法/main.cpp
@@ -1,69 +1,61 @@
-// KMP算法是用于检查模式串是否存在于目标串的一种算法 这种算法的时间复杂度较低
-// 获得前缀表
-#include <string>
-#include <iostream>
-void getNext(int *Next, std::string str)
-{
-    // 初始化Next列表
-    int j = 0, i; // j是前缀的指针 i是后缀的指针
-    Next[j] = 0;
-    for (i = 1; i < str.size(); i++)
-    {
-        // 前缀不等于后缀
-        while (j > 0 && str[i] != str[j])
-        {
-            j = Next[j - 1];
-        }
-        // 前缀等于后缀
-        if (str[i] == str[j])
-        {
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+void getNext(string pattern, int next[]) {
+    next[0] = -1;//下标零默认为 -1
+    int i = 1, j = 0;
+    while (i < pattern.size() || j < pattern.size()) {
+        if (j == -1 || pattern[i] == pattern[j]) {
             j++;
+            next[i] = j;
         }
-        // 保存 如果一直不相等 j会在while循环中变为0 不用担心
-        Next[i] = j;
+        else {
+            j = next[j];
+        }
     }
 }
-// 返回查找到的字串下标 若无 则返回-1
 
-int PMK_Search(const std::string &target, const std::string &pattern)
-{
-    if (pattern.empty())
-        return 0; // 约定：空模式视为在位置0匹配
 
-    // int n = (int)target.size();
-    // int m = (int)pattern.size();
-    std::vector<int> Next(pattern.size());
-    getNext(Next.data(), pattern);
-    int i = 0, j = 0;
-    while (i < target.size())
-    {
-        if (target[i] == pattern[j])
-        {
+int kmp(string s, string p, int next[]) {
+    int i = 0;
+    int j = 0;
+    int sLen = s.size();
+    int pLen = p.size();
+
+    while (i < sLen && j < pLen) {
+        if (j == -1 || s[i] == p[j]) {
             i++;
             j++;
         }
-        else if (j > 0)
-        {
-            j = Next[j - 1];
+        else {
+            j = next[j];
         }
-        else
-        {
-            i++;
-        }
-        if (j == pattern.size())
-            return i - j;
     }
+
+    if (j == pLen) return i - j;
     return -1;
 }
 
-int main()
-{
-    std::string str1 = "aabaaabaaac";
-    std::vector<int> next(str1.size());
-    getNext(next.data(), str1);
-    std::string str2 = "aabaaac";
-    std::cout << PMK_Search(str1, str2) << std::endl;
+
+
+
+int main() {
+    string pattern;
+    string s;
+    cout << "请输入主串:";
+    getline(cin, s);
+    cout << "请输入主串:";
+    getline(cin, pattern);
+    int* next = new int[pattern.size()];
+
+    getNext(pattern, next);
+    for (int i = 0;i < pattern.size();i++) {
+        cout << next[i] << " ";
+    }
+    cout << endl;
+
+    cout << kmp(s, pattern, next) << endl;
     return 0;
 }
-
-// 时间复杂度O(m+n)
